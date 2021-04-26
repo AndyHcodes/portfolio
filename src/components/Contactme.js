6cc09bebf8eda40bc6a2a05bde3de0258886a8ef
AndyHcodes <67926150+AndyHcodes@users.noreply.github.com>
2021-04-26 22:11:12 +0100

    formspree used to create contact me form

--- src/components/Contactme.js
+++ src/components/Contactme.js
@@ -1,24 +1,32 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import "../styles/Contactme.css";
 
 function Contact() {
+  const [state, handleSubmit] = useForm("mnqlnlwz");
+  if (state.succeeded) {
+    return <p className="success-message">Thank you for your message</p>;
+  }
   return (
     <div className="contact-form-container">
       <h1>Contact Me</h1>
-      <div className="contact-details">
-        <ul>
-          <i className="fa fa-phone-square" ari-hidden="true" />
-          <span>07855018590</span>
-        </ul>
-        <ul>
-          <i className="fa fa-envelope" ari-hidden="true" />
-          <span>andyhcodes@gmail.com</span>
-        </ul>
-        <ul>
-          <i className="fa fa-address-book" ari-hidden="true" />
-          <span>Sale, Manchester</span>
-        </ul>
-      </div>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" name="name" />
+        <label htmlFor="email">Email Address:</label>
+        <input id="email" type="email" name="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <label htmlFor="text">Type your message here:</label>
+        <textarea id="message" name="message" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
